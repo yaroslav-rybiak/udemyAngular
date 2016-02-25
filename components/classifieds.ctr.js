7069764d450415dc5d6df4b711e83bda9d7864cf
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2016-02-25 12:03:55 +0200

    filters

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -9,7 +9,7 @@
                     "title": "Columbia jacket",
                     "description": "Take back the outdoors with Columbia sportswear women's jacket shells.",
                     "price": 400,
-                    "posted:": "14.02.2016",
+                    "posted": "14.02.2016",
                     "contact": {
                         "name": "Diane S. Crocker",
                         "phone":"920-269-2259",
@@ -22,7 +22,7 @@
                     "title": "Ecco boots",
                     "description": "A favorite rugged boot that offers classic style and great comfort.",
                     "price": 600,
-                    "posted:": "10.02.2016",
+                    "posted": "10.02.2016",
                     "contact": {
                         "name": "David E. Rast",
                         "phone":"248-224-6305",
@@ -35,7 +35,7 @@
                     "title": "Skechers shoes",
                     "description": "Supreme comfort and sporty style gives you the edge in the skechers shoes.",
                     "price": 350,
-                    "posted:": "07.02.2016",
+                    "posted": "07.02.2016",
                     "contact": {
                         "name": "Sarah J. Nunn",
                         "phone":"732-830-3499",
@@ -48,7 +48,7 @@
                     "title": "Bond girl shorts",
                     "description": "Get equipped for outdoors in warm conditions with these stretch woven shorts.",
                     "price": 220,
-                    "posted:": "06.02.2016",
+                    "posted": "06.02.2016",
                     "contact": {
                         "name": "John D. Watson",
                         "phone":"774-253-0243",
@@ -61,7 +61,7 @@
                     "title": "Reindeer sweater",
                     "description": "Intarisa-knit wool, ribbed crew neckline, long sleeves with ribbed cuffs.",
                     "price": 285,
-                    "posted:": "03.02.2016",
+                    "posted": "03.02.2016",
                     "contact": {
                         "name": "Katherine B. Wagner",
                         "phone":"334-309-9613",
@@ -74,7 +74,7 @@
                     "title": "Low cut toed socks",
                     "description": "Five pairs of stylish low cut toed cotton socks.",
                     "price": 15,
-                    "posted:": "30.01.2016",
+                    "posted": "30.01.2016",
                     "contact": {
                         "name": "Leonard P. Smith",
                         "phone":"303-450-1661",
