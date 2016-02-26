94f715ac843c3e92a3c79f87a6996ef25043e8f3
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2016-02-26 12:30:15 +0200

    sidenav

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -2,9 +2,19 @@
     "use strict";
     angular
         .module("ngClassifieds")
-        .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory) {
+        .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav) {
             classifiedsFactory.getClassifieds().then(function(classifieds){
                 $scope.classifieds = classifieds.data;
-            })
+            });
+
+            $scope.openSidebar = function() {
+                $mdSidenav('left').open();
+            };
+
+            $scope.closeSidebar = function() {
+                $mdSidenav('left').close();
+            };
+
         });
+
 })();
