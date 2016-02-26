199c4ced6e97e257077ffaf8319d920e32a8a743
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2016-02-26 10:46:45 +0200

    factory

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -2,8 +2,8 @@
     "use strict";
     angular
         .module("ngClassifieds")
-        .controller("classifiedsCtrl", function($scope, $http) {
-            $http.get('../ngClassifieds/data/classifieds.json').then(function(classifieds){
+        .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory) {
+            classifiedsFactory.getClassifieds().then(function(classifieds){
                 $scope.classifieds = classifieds.data;
             })
         });
